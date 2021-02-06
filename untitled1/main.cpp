533ad470d83297ccf577cec4f32e8676fb5d20bd
Toradix <dawid.podsiadly@interia.pl>
2021-02-06 17:27:10 +0100

    zmiana na mnożenie w metodzie policz

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 Init Policz (int a, int b) {
-    Return a*b;
+    return (a+b)*(a-b);
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std:: cout <<"W drugim branchu; 2*3=" << (Policz (2,3)) << std::endl;
+    std:: cout <<"W drugim branchu; 2+3=" << Policz (2,3) << std::endl;
     return 0;
 }
